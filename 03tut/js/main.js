f87506782694baed7cc2830656d415c2ee7577da
Sebastian Franco Ulloa <sebastian.ulloa@xal.no>
2024-04-30 13:43:06 +0200

    finished 03tut

---
+++ 03tut/js/main.js
@@ -0,0 +1,27 @@
+// strings
+const myVariable = "Mathematics";
+
+// the length property
+console.log(myVariable.length);
+
+// charAt method
+console.log(myVariable.charAt(3));
+
+// indexOf method
+console.log(myVariable.indexOf("the"));
+
+// lastIndexOf method
+console.log(myVariable.lastIndexOf("at"));
+
+// slice method
+console.log(myVariable.slice(2, 5));
+
+// toUpperCase and toLowerCase methods
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+
+// include method
+console.log(myVariable.includes("math"));
+
+// split method
+console.log(myVariable.split("t"));
