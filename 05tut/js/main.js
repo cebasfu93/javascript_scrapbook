d29c377b305ee968914b12b3bc695151aa88feee
Sebastian Franco Ulloa <sebastian.ulloa@xal.no>
2024-05-02 08:59:51 +0200

    finished 05tut

---
+++ 05tut/js/main.js
@@ -0,0 +1,15 @@
+console.log(Math.PI);
+
+console.log(Math.trunc(Math.PI));
+
+console.log(Math.round(3.6));
+
+console.log(Math.ceil(3.2));
+
+console.log(Math.floor(3.8));
+
+console.log(Math.pow(2, 3));
+
+console.log(Math.min(1, 2, 3));
+
+console.log(Math.max(1, 2, 3));
