468ef99a647573494b91570331e1a1bd80869504
Sebastian Franco Ulloa <sebastian.ulloa@xal.no>
2024-05-02 11:02:39 +0200

    finished 10tut

---
+++ 10tut/js/main.js
@@ -0,0 +1,9 @@
+// User input
+
+// alert("Hello World!");
+
+// let myBoolean = confirm("Ok === True\nCancel === False");
+// console.log(myBoolean);
+
+let name = prompt("Please enter your name.");
+console.log(name);
