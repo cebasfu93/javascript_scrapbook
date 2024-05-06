688b86b10356502998a5a3cc32da8d7c13836846
Sebastian Franco Ulloa <sebastian.ulloa@xal.no>
2024-05-06 11:01:24 +0200

    finished 25tut

---
+++ 25tut/js/posts.js
@@ -0,0 +1,26 @@
+export const posts = [
+  {
+    userId: 1,
+    id: 1,
+    title: "Lorem ipsum dolor sit amet",
+    body: "consectetur adipisicing elit. Et officiis nam quam nulla ad mollitia enim eligendi pariatur unde in magnam, id tempore similique quos quod earum assumenda laudantium itaque?",
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "Lorem ipsum dolor sit amet",
+    body: "consectetur adipisicing elit. Et officiis nam quam nulla ad mollitia enim eligendi pariatur unde in magnam, id tempore similique quos quod earum assumenda laudantium itaque?",
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "Lorem ipsum dolor sit amet",
+    body: "consectetur adipisicing elit. Et officiis nam quam nulla ad mollitia enim eligendi pariatur unde in magnam, id tempore similique quos quod earum assumenda laudantium itaque?",
+  },
+  {
+    userId: 2,
+    id: 4,
+    title: "Lorem ipsum dolor sit amet",
+    body: "consectetur adipisicing elit. Et officiis nam quam nulla ad mollitia enim eligendi pariatur unde in magnam, id tempore similique quos quod earum assumenda laudantium itaque?",
+  },
+];
